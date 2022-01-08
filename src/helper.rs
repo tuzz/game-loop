8fd5cff8ba54e76f95acb96c2f50a5b23a461551
Jay Oster <jay@kodewerx.org>
2022-01-08 02:45:02 -0800

    Allow using winit user events

--- src/helper.rs
+++ src/helper.rs
@@ -59,11 +59,12 @@ mod helper {
 
     pub use winit;
 
-    pub fn game_loop<G, U, R, H>(event_loop: EventLoop<()>, window: Window, game: G, updates_per_second: u32, max_frame_time: f64, mut update: U, mut render: R, mut handler: H) -> !
+    pub fn game_loop<G, U, R, H, T>(event_loop: EventLoop<T>, window: Window, game: G, updates_per_second: u32, max_frame_time: f64, mut update: U, mut render: R, mut handler: H) -> !
         where G: 'static,
               U: FnMut(&mut GameLoop<G, Time, Window>) + 'static,
               R: FnMut(&mut GameLoop<G, Time, Window>) + 'static,
-              H: FnMut(&mut GameLoop<G, Time, Window>, Event<()>) + 'static,
+              H: FnMut(&mut GameLoop<G, Time, Window>, Event<'_, T>) + 'static,
+              T: 'static,
     {
         let mut game_loop = GameLoop::new(game, updates_per_second, max_frame_time, window);
 
