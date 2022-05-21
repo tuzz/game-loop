3a66fc6fe6f3fd5a22005b0f4952293416c8455a
Matt <matt328@users.noreply.github.com>
2022-05-21 13:44:30 -0400

    Changes Window usage to Arc<Window>

--- src/helper.rs
+++ src/helper.rs
@@ -52,6 +52,7 @@ mod helper {
 
 #[cfg(feature="winit")]
 mod helper {
+    use std::sync::Arc;
     use super::*;
     use winit::event::Event;
     use winit::event_loop::{ControlFlow, EventLoop};
@@ -59,11 +60,11 @@ mod helper {
 
     pub use winit;
 
-    pub fn game_loop<G, U, R, H, T>(event_loop: EventLoop<T>, window: Window, game: G, updates_per_second: u32, max_frame_time: f64, mut update: U, mut render: R, mut handler: H) -> !
+    pub fn game_loop<G, U, R, H, T>(event_loop: EventLoop<T>, window: Arc<Window>, game: G, updates_per_second: u32, max_frame_time: f64, mut update: U, mut render: R, mut handler: H) -> !
         where G: 'static,
-              U: FnMut(&mut GameLoop<G, Time, Window>) + 'static,
-              R: FnMut(&mut GameLoop<G, Time, Window>) + 'static,
-              H: FnMut(&mut GameLoop<G, Time, Window>, &Event<'_, T>) + 'static,
+              U: FnMut(&mut GameLoop<G, Time, Arc<Window>>) + 'static,
+              R: FnMut(&mut GameLoop<G, Time, Arc<Window>>) + 'static,
+              H: FnMut(&mut GameLoop<G, Time, Arc<Window>>, &Event<'_, T>) + 'static,
               T: 'static,
     {
         let mut game_loop = GameLoop::new(game, updates_per_second, max_frame_time, window);
